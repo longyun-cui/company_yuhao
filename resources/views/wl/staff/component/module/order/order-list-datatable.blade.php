cd7dbd38eacce5a2c69f4d100172d8c66a2fa0fe
longyun-cui <longyun.cui88@gmail.com>
2026-04-02 12:53:49 +0800

    staff

--- resources/views/wl/staff/component/module/order/order-list-datatable.blade.php
+++ resources/views/wl/staff/component/module/order/order-list-datatable.blade.php
@@ -707,7 +707,7 @@
                     "title": "出发地",
                     "data": "transport_departure_place",
                     "className": "",
-                    "width": "80px",
+                    "width": "120px",
                     "orderable": false,
                     "fnCreatedCell": function (nTd, data, row, iRow, iCol) {
                         if(row.is_completed != 1)
@@ -729,7 +729,7 @@
                     "title": "目的地",
                     "data": "transport_destination_place",
                     "className": "",
-                    "width": "80px",
+                    "width": "120px",
                     "orderable": false,
                     "fnCreatedCell": function (nTd, data, row, iRow, iCol) {
                         if(row.is_completed != 1)
