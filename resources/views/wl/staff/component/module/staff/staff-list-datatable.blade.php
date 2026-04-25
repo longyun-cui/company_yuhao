fc0ce692feb6ff0ed66cc130c9b73a035c28b504
longyun-cui <longyun.cui88@gmail.com>
2026-04-25 10:49:25 +0800

    staff

--- resources/views/wl/staff/component/module/staff/staff-list-datatable.blade.php
+++ resources/views/wl/staff/component/module/staff/staff-list-datatable.blade.php
@@ -6,7 +6,7 @@
         let $tableSearch = $datatable_wrapper.find('.datatable-search-box');
 
         $($tableId).DataTable({
-            "aLengthMenu": [[10, 50, 100, 200, -1], ["10", "50", "100", "200", "全部"]],
+            "aLengthMenu": [[20, 50, 100, 200, -1], ["20", "50", "100", "200", "全部"]],
             "processing": true,
             "serverSide": true,
             "searching": false,
